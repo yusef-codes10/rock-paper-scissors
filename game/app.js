5511d49d78b6b1eabd9b71cd4d4581f6cf63bf3a
Youcef <youcefcopy@gmail.com>
2025-12-04 17:47:04 +0100

    main functions |  3 main (rock, paper, scissors), hidden functions (cpuChoice() & gameResult())

--- game/app.js
+++ game/app.js
@@ -8,3 +8,23 @@ const scissorsBtn = document.getElementById('scissorsBtn');
 const rock = 'rock';
 const scissors = 'scissors';
 const paper = 'paper';
+
+function cpuChoice() {
+    // get a random choice
+}
+
+function gameResult() {
+    // compares player vs cpu
+}
+
+function rockChoice() {
+    // calls cpuChoice() and sets userChoice as rock
+}
+
+function paperChoice() {
+    // calls cpuChoice() and sets userChoice as paper
+}
+
+function scissorsChoice() {
+    // calls cpuChoice() and sets userChoice as scissors
+}
