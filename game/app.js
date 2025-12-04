f1c5e735d78772947fb0212576842ae042e9ea08
Youcef <youcefcopy@gmail.com>
2025-12-04 17:41:54 +0100

    necessary html elements

--- game/app.js
+++ game/app.js
@@ -1,1 +1,7 @@
 console.log('js loaded');
+
+const gameDisplay = document.querySelector('.game-display');
+const rockBtn = document.getElementById('rockBtn');
+const paperBtn = document.getElementById('paperBtn');
+const scissorsBtn = document.getElementById('scissorsBtn');
+
